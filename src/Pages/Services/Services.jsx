cf28216ad1edfa8bb97b9c3da8d7f3125d70bed1
Hasibul Hasan <hasibulhasan442@gmail.com>
2022-03-08 23:22:46 +0600

    Data Fatch From Laravel Api

--- src/Pages/Services/Services.jsx
+++ src/Pages/Services/Services.jsx
@@ -1,19 +1,53 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 function Services() {
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [items, setItems] = useState([]);
+  useEffect(() => {
+    fetch("http://127.0.0.1:8000/api/userlist")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          setIsLoaded(true);
+          setItems(result);
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      )
+  }, [])
+
   return (
     <>
-    <h1>Service</h1>
-    <h1>Service</h1>
-    <h1>Service</h1>
-    <h1>Service</h1>
-    <h1>Service</h1>
-    <h1>Service</h1>
-    <h1>Service</h1>
-    <h1>Service</h1>
-    <h1>Service</h1>
-    <h1>Service</h1>
-    <h1>Service</h1>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+     <table className="table">
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Name</th>
+                    <th>Email</th>
+                </tr>
+                
+            </thead>
+            <tbody>
+                {items.map(item => (
+                  <tr key={item.id}>
+                    <td>{item.id}</td>
+                    <td>{item.name}</td>
+                    <td>{item.email}</td>
+                  </tr>
+                ))}
+            </tbody>
+        </table>
     </>
   )
 }

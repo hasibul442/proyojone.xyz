a463a0d32a1bb50403d1159d0a73fa3c92e69903
Hasibul Hasan <hasibulhasan442@gmail.com>
2022-03-08 23:23:26 +0600

    Data Fatch From Laravel Api

--- src/Pages/Services/Services.jsx
+++ src/Pages/Services/Services.jsx
@@ -12,9 +12,7 @@ function Services() {
           setIsLoaded(true);
           setItems(result);
         },
-        // Note: it's important to handle errors here
-        // instead of a catch() block so that we don't swallow
-        // exceptions from actual bugs in components.
+
         (error) => {
           setIsLoaded(true);
           setError(error);
